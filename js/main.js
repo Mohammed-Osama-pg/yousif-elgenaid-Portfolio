3041552a82838cf3eab87c06ed8d779af37f25c5
Mohammed-Osama-pg <141535887+Mohammed-Osama-pg@users.noreply.github.com>
2025-03-18 21:54:28 +0300

    Revert "Add smooth scrolling"
    This reverts commit 85490be75c04d065a683e6905f7d2f94f208e9a2.

--- js/main.js
+++ js/main.js
@@ -85,10 +85,3 @@ window.addEventListener("load", () => {
     ele.style.animationPlayState = "running";
   });
 });
-
-// !-- LocomotiveScroll ----------------------------------
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("body"), // Target your main wrapper
-  smooth: true,
-  lerp: 0.05, // Lower = smoother, higher = faster
-});
