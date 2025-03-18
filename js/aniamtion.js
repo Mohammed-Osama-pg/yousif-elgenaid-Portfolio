999cfa142155c850c365c8bb9a8cbefe03b51885
Mohammed-Osama-pg <141535887+Mohammed-Osama-pg@users.noreply.github.com>
2025-03-18 14:36:23 +0300

    Scroll animation

---
+++ js/aniamtion.js
@@ -0,0 +1,76 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".fade-in-down", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".fade-in-down", // Element that triggers animation
+    start: "top 90%", // Start when top of .box reaches 80% of viewport
+    end: "top 30%", // End when top of .box reaches 30% of viewport
+    scrub: true, // Smooth animation as user scrolls
+  },
+});
+gsap.from(".fade-in-left", {
+  opacity: 0,
+  x: -50,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".fade-in-left", // Element that triggers animation
+    start: "top 90%", // Start when top of .box reaches 80% of viewport
+    end: "top 30%", // End when top of .box reaches 30% of viewport
+    scrub: true, // Smooth animation as user scrolls
+  },
+});
+gsap.to(".show-text", {
+  width: 0,
+  scrollTrigger: {
+    trigger: ".show-text", // Element that triggers animation
+    start: "top 80%", // Start when top of .box reaches 80% of viewport
+    end: "top -100%", // End when top of .box reaches 30% of viewport
+    scrub: true, // Smooth animation as user scrolls
+  },
+});
+
+gsap.from(".show", {
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".show", // Element that triggers animation
+    start: "top 80%", // Start when top of .box reaches 80% of viewport
+    end: "top 20%", // End when top of .box reaches 30% of viewport
+    scrub: true, // Smooth animation as user scrolls
+  },
+});
+
+gsap.from(".slide-left", {
+  opacity: 0,
+  x: "-110%",
+  scaleX: 0,
+  scrollTrigger: {
+    trigger: ".slide-left", // Element that triggers animation
+    start: "top 80%", // Start when top of .box reaches 80% of viewport
+    end: "top 20%", // End when top of .box reaches 30% of viewport
+    scrub: true, // Smooth animation as user scrolls
+  },
+});
+gsap.from(".slide-right", {
+  opacity: 0,
+  x: "110%",
+  scaleX: 0,
+  scrollTrigger: {
+    trigger: ".slide-right", // Element that triggers animation
+    start: "top 80%", // Start when top of .box reaches 80% of viewport
+    end: "top 20%", // End when top of .box reaches 30% of viewport
+    scrub: true, // Smooth animation as user scrolls
+  },
+});
+gsap.from(".slide-center", {
+  opacity: 0,
+  scaleX: 0,
+  scrollTrigger: {
+    trigger: ".slide-center", // Element that triggers animation
+    start: "top 80%", // Start when top of .box reaches 80% of viewport
+    end: "top 20%", // End when top of .box reaches 30% of viewport
+    scrub: true, // Smooth animation as user scrolls
+  },
+});
